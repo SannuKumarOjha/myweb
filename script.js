0e86c1ac2786fbff76b45824b5a6af755cdb96b7
Sannu Kumar Ojha <137702131+SannuKumarOjha@users.noreply.github.com>
2023-07-16 19:49:24 +0530

    index.html , style.css , script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+// GSAP Code In Js //
+
+gsap.from('h2' ,{
+    x : 200 ,
+    y : 200 ,
+    duration : .5 ,
+    delay : 0.2 ,
+    rotate : 15 ,
+})
+
+gsap.from('.upper' ,{
+    x : 700 ,
+    y : 200 ,
+    duration : .7 ,
+    delay : 0.4 ,
+})
+
+gsap.from('.lower' ,{
+    x : 700 ,
+    y : -200 ,
+    duration : .7 ,
+    delay : 0.4 ,
+})
+
+gsap.from('.bottom' ,{
+    x : 300 ,
+    y : 500 ,
+    duration : .9 ,
+    delay : 0.4 ,
+})
+
+gsap.from('.btn' ,{
+    x : 900 ,
+    y : 800 ,
+    duration : .9 ,
+    delay : 0.6 ,
+})
+
+gsap.from('.page1_right' ,{
+    x : 400 ,
+    y : -300 ,
+    duration : .4 ,
+    delay : 0.4 ,
+})
+
+gsap.from('.first , .Second' ,{
+    x : 100 ,
+    y : 0 ,
+    duration : 1 ,
+    delay : 2.4 ,
+    fontSize : 2
+})
+
+gsap.from('.boxes' ,{
+    x : -300 ,
+    y : 400 ,
+    duration : 1 ,
+    delay : 2 ,
+})
+
+//  <<<<==============================================>>>>
